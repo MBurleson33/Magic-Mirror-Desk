5a3b77e6f2873f3af4a10cd90587bf387a985310
MBurleson33 <114445009+MBurleson33@users.noreply.github.com>
2026-03-07 23:41:10 -0600

    Update config.js

--- config.js
+++ config.js
@@ -38,71 +38,32 @@ let config = {
 	units: "metric",
 
 	modules: [
-		{
-			module: "alert",
-		},
-		{
-			module: "updatenotification",
-			position: "top_bar"
-		},
-		{
-			module: "clock",
-			position: "top_left"
-		},
-		{
-			module: "calendar",
-			header: "US Holidays",
-			position: "top_left",
-			config: {
-				calendars: [
-					{
-						fetchInterval: 7 * 24 * 60 * 60 * 1000,
-						symbol: "calendar-check",
-						url: "https://ics.calendarlabs.com/76/mm3137/US_Holidays.ics"
-					}
-				]
-			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third"
-		},
-		{
-			module: "weather",
-			position: "top_right",
-			config: {
-				weatherProvider: "openmeteo",
-				type: "current",
-				lat: 40.776676,
-				lon: -73.971321
-			}
-		},
-		{
-			module: "weather",
-			position: "top_right",
-			header: "Weather Forecast",
-			config: {
-				weatherProvider: "openmeteo",
-				type: "forecast",
-				lat: 40.776676,
-				lon: -73.971321
-			}
-		},
-		{
-			module: "newsfeed",
-			position: "bottom_bar",
-			config: {
-				feeds: [
-					{
-						title: "New York Times",
-						url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true,
-				broadcastNewsFeeds: true,
-				broadcastNewsUpdates: true
-			}
+    {
+        module: "clock",
+        position: "top_left"
+    },
+    {
+        module: "weather",
+        position: "top_right",
+        config: {
+            weatherProvider: "openmeteo",
+            type: "current",
+            lat: 35.4676,
+            lon: -97.5164
+        }
+    },
+    {
+        module: "weather",
+        position: "top_right",
+        header: "Weather Forecast",
+        config: {
+            weatherProvider: "openmeteo",
+            type: "forecast",
+            lat: 35.4676,
+            lon: -97.5164
+        }
+    },
+]
 		},
 	]
 };
