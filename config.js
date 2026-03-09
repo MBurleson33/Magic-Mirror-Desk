d53fbf1a05c5258758c37584f0bbb9592b090f89
MBurleson33 <114445009+MBurleson33@users.noreply.github.com>
2026-03-09 08:10:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -58,7 +58,7 @@ let config = {
 				source: "apod-hd",
 				slideInterval: 5 * 60 * 1000,
 				maximumEntries: 10,
-				filter: "brightness(0.125)"
+				filter: "grayscale(100%) brightness(0.125)"
 			}
 		},
 		{
