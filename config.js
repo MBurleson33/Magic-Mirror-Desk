96ad2d7b74ccb7752bedb3da1330d7e1bc2b1db9
MBurleson33 <114445009+MBurleson33@users.noreply.github.com>
2026-03-09 08:12:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ let config = {
 			module: "MMM-Wallpaper",
 			position: "fullscreen_below",
 			config: {
-				source: "apod-hd",
+				source: "/r/spaceporn",
 				slideInterval: 5 * 60 * 1000,
 				maximumEntries: 10,
 				filter: "grayscale(100%) brightness(0.125)"
